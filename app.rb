f5e665adae2c30a9114701100130aaa347998fbf
Micah Banschick <micha.ban@gmail.com>
2020-07-23 17:27:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get /name do 
+  end 
+  
+  get /name do 
+  end
+  
+  get /name do 
+  end
+  
+  get /name do 
+  end
+  
 end
